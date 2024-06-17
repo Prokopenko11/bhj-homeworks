af98a9c088ee513ae9a3ac6ac22efd379aff6f72
Valeria Prokopenko <valeriaprokopenko59@gmail.com>
2024-06-18 01:29:37 +0300

    ads initial commit

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+function startRotator(rotatorElement) {
+  const rotatorCases = rotatorElement.querySelectorAll('.rotator__case');
+  let currentIndex = 0;
+
+  setInterval(() => {
+    rotatorCases[currentIndex].classList.remove('rotator__case_active');
+  
+    if (currentIndex < rotatorCases.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+  
+    rotatorCases[currentIndex].classList.add('rotator__case_active');
+  }, 1000);
+}
+
+const rotators = document.querySelectorAll('.rotator');
+rotators.forEach(startRotator)
+
